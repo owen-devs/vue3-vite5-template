dc948da7b9e0a3f4511dce678a56126257aceba4
Owen <411231129@qq.com>
2024-11-24 02:39:10 +0800

    feat：超出省略显示tooltips组件

---
+++ src/components/AutoTooltips.vue
@@ -0,0 +1,61 @@
+<template>
+    <el-tooltip placement="top" :disabled="!props.text || isDisabled" v-bind="$attrs">
+        <template #content>
+            <div class="" v-text="props.text"></div>
+        </template>
+        <div
+            class="w-full"
+            :class="
+                [
+                    $attrs.class,
+                    { truncate: !props.rows || props.rows === 1, 'multi-lines': props.rows > 1 }
+                ].concat(props.classNames)
+            "
+            ref="wrapper"
+            v-text="props.text"
+        ></div>
+    </el-tooltip>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+    text: {
+        type: String,
+        default: ''
+    },
+    classNames: {
+        type: Array,
+        default: () => []
+    },
+    rows: {
+        type: Number,
+        default: 1
+    }
+})
+
+const wrapper = ref()
+const isDisabled = ref(true)
+const setDisabled = () => {
+    console.log(wrapper.value.offsetHeight)
+    console.log(wrapper.value.scrollHeight)
+    isDisabled.value =
+        !props.rows || props.rows === 1
+            ? wrapper.value.offsetWidth >= wrapper.value.scrollWidth
+            : wrapper.value.offsetHeight >= wrapper.value.scrollHeight
+
+    console.log(isDisabled.value)
+}
+
+onUpdated(() => {
+    setDisabled()
+})
+</script>
+<style lang="scss" scoped>
+.multi-lines {
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    line-clamp: v-bind('rows');
+    -webkit-line-clamp: v-bind('rows');
+    -webkit-box-orient: vertical;
+}
+</style>
---
+++ src/views/compoDemo/autoTooltips/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="w-100px">
+        <AutoTooltips :text="text1" class="mb-10px" />
+        <AutoTooltips :text="text2" class="mb-10px" />
+        <AutoTooltips :text="text3" :rows="3" class="mb-10px" />
+        <AutoTooltips :text="text4" :rows="3" class="mb-10px" />
+        <AutoTooltips text="" :rows="3" class="mb-10px" />
+    </div>
+</template>
+<script lang="ts" setup>
+const text1 = ref()
+const text2 = ref()
+const text3 = ref()
+const text4 = ref()
+
+setTimeout(() => {
+    text1.value = '地方都是发收'
+    text2.value = '地方都是发收的发生的发生的'
+    text3.value = '地方都是发收的发生的发生的打的'
+    text4.value =
+        '地方都是发收的发生的发生的打发打发打发打发地方都是发收的发生的发生的打发打发打发打发水电费发生的发生的发发水电费发生的发生的发发生的发生的'
+}, 3300)
+</script>
+<style lang="scss" scoped></style>
